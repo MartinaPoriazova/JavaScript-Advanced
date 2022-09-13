ed1db8b7d7e2e086c780ccd6d326fcbd7d121ee1
MartinaPoriazova <102858702+MartinaPoriazova@users.noreply.github.com>
2022-09-13 23:36:00 +0300

    Add second solution.

--- 04_Exercise Syntax_Functions_Statements/02_GreatestCommonDivisor.js
+++ 04_Exercise Syntax_Functions_Statements/02_GreatestCommonDivisor.js
@@ -3,7 +3,7 @@ function greatestCommonDivisor(num1, num2) {
     let min = Math.min(num1, num2);
 
     for (let i = 2; i <= min; i++) {
-        if (num1 % i === 0 && num2 % i == 0) {
+        if (num1 % i == 0 && num2 % i == 0) {
             gcd = i;
         }
     }
