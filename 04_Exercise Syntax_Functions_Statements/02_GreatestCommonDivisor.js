2d1132ad5775a90521562c68e2299bd0f1695a56
MartinaPoriazova <102858702+MartinaPoriazova@users.noreply.github.com>
2022-09-13 23:31:10 +0300

    Update 02_GreatestCommonDivisor.js

---
+++ 04_Exercise Syntax_Functions_Statements/02_GreatestCommonDivisor.js
@@ -0,0 +1,27 @@
+function greatestCommonDivisor(num1, num2) {
+    let gcd = 1;
+    let min = Math.min(num1, num2);
+
+    for (let i = 2; i <= min; i++) {
+        if (num1 % i === 0 && num2 % i == 0) {
+            gcd = i;
+        }
+    }
+
+    console.log(gcd);
+}
+
+greatestCommonDivisor(15,5);
+greatestCommonDivisor(2154, 458);
+
+function solve(x, y) {
+    while(y) {
+        let temp = y;
+        y = x % y;
+        x = temp;
+    }
+    return x;
+}
+
+console.log(solve(15, 5));
+console.log(solve(2154, 458));
