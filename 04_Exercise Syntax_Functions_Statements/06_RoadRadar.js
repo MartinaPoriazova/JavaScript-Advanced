ee48d699519c8f6f5e17ef40318be48371999f0c
MartinaPoriazova <102858702+MartinaPoriazova@users.noreply.github.com>
2022-09-14 21:57:57 +0300

    Update 06_RoadRadar.js

--- 04_Exercise Syntax_Functions_Statements/06_RoadRadar.js
+++ 04_Exercise Syntax_Functions_Statements/06_RoadRadar.js
@@ -9,12 +9,23 @@ function roadRadar(speed, area) {
     }
 
     let difference = speed - limit;
+    let status;
 
     if(speed <= limit) {
         console.log(`Driving ${speed} km/h in a ${limit} zone`)
     } else {
-        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${limit} - {status}`)
+        if(difference <= 20) {
+            status = "speeding";
+        } else if (difference > 20 && difference <= 40) {
+            status = "excessive speeding";
+        } else {
+            status = "reckless driving";
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${status}`)
     }
-
-
 }
+
+roadRadar(40, 'city');
+roadRadar(21, 'residential');
+roadRadar(121, 'interstate');
+roadRadar(200, 'motorway');
