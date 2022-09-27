014e0fe7dfa80045af0e2f4f26f515092b7a4a0e
MartinaPoriazova <102858702+MartinaPoriazova@users.noreply.github.com>
2022-09-27 17:19:29 +0300

    Initial commit.

--- 11_DOM_Manipulations_And_Events/03. Add-Delete/app.js
+++ 11_DOM_Manipulations_And_Events/03. Add-Delete/app.js
@@ -1,29 +1,43 @@
+// function addItem() {
+//     let ul = document.getElementById('items');
+
+//     let input = document.getElementById('newItemText');
+//     let value = input.value;
+
+//     if(value.length === 0) {
+//         return;
+//     }
+
+//     let li = document.createElement('li');
+//     li.textContent = value;
+
+//     let deleteBtn = document.createElement('a');
+//     deleteBtn.textContent = '[Delete]';
+
+//     deleteBtn.addEventListener('click', function (event) {
+//         event.target.parentElement.remove();
+//     });
+
+//     li.appendChild(deleteBtn);
+
+//     ul.append(li);
+
+//     input.value = '';
+// }
+
 function addItem() {
-    let ul = document.getElementById('items');
+    let list = document.getElementById('items')
+    list.addEventListener('click', deleteItem)
 
-    let input = document.getElementById('newItemText');
-    let value = input.value;
+    let newItemText = document.getElementById('newText')
+    let newItem = document.createElement('li')
+    newItem.innerHTML = `${newItemText.value} <a href="#:">[Delete]</a>`
+    list.appendChild(newItem)
+    newItemText.value = ''
 
-    if(value.length === 0) {
-        return;
+    function deleteItem(item) {
+        let element = item.target.parentNode
+        element.remove()
     }
-
-    let li = document.createElement('li');
-    li.textContent = value;
-
-    let deleteBtn = document.createElement('a');
-    deleteBtn.textContent = '[Delete]';
-
-    deleteBtn.addEventListener('click', function (event) {
-        event.target.parentElement.remove();
-    });
-
-    li.append(deleteBtn);
-
-    ul.append(li);
-
-    input.value = '';
 }
-
-
     
