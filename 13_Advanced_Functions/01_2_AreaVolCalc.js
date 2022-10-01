b449ab7521cb0ed0ceaa33cbddfcdd1067bbc8c5
MartinaPoriazova <102858702+MartinaPoriazova@users.noreply.github.com>
2022-10-01 15:59:39 +0300

    Create 01_2_AreaVolCalc.js

---
+++ 13_Advanced_Functions/01_2_AreaVolCalc.js
@@ -0,0 +1,20 @@
+function solve(area, vol, input) {
+    let objects = JSON.parse(input);
+
+    function calc(obj) {
+      let areaObj = Math.abs(area.call(obj));
+      let volumeObj = Math.abs(vol.call(obj));
+
+      return { area: areaObj, volume: volumeObj }
+    }
+    return objects.map(calc);
+  }
+
+let area = function area() {
+    return Math.abs(this.x * this.y);
+};
+
+let vol = function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
