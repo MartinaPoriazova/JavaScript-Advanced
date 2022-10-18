63b761b98b3b3520c3668aa548783e3ded50febe
MartinaPoriazova <102858702+MartinaPoriazova@users.noreply.github.com>
2022-10-18 15:35:31 +0300

    Update library.test.js

--- Old_Exams/Ex_231022/03. Library/test/library.test.js
+++ Old_Exams/Ex_231022/03. Library/test/library.test.js
@@ -1,5 +1,6 @@
 let library = require('../library');
 let {assert} = require('chai');
+const { findBook } = require('../library');
 
 describe("Tests for library", () => {
     describe("Test calcPriceOfBook(nameOfBook, year)", () => {
@@ -28,13 +29,42 @@ describe("Tests for library", () => {
         });
     });
 
-//TO DO:
+    describe("Test findBook(booksArr, desiredBook)", () => {
+        it('should throw a new Error if the bookArr is empty', () => {
+            let errMsg = `No books currently available`;
+            assert.throw(() => library.findBook([], "Troy"), errMsg);
+        });
+        it('should return a message that the book is presented in the bookArr', () => {
+            let returnMsg = `We found the book you want.`;
+            assert.equal(library.findBook(["Troy", "Life Style", "Torronto"], "Troy"), returnMsg);
+        });
+        it('should throw a new Error if the book is not presented in the bookArr', () => {
+            let errMsg = `The book you are looking for is not here!`;
+            assert.equal(library.findBook(["Troy", "Life Style", "Torronto"], "Lollita"), errMsg);
+        });
+    });
 
-
-
-
-
-
-
-
+    describe("Test arrangeTheBooks(countBooks)", () => {
+        it('should throw a new Error if input is not valid', () => {
+            let errMsg = `Invalid input`;
+            assert.throw(() => library.arrangeTheBooks("5"), errMsg);
+            assert.throw(() => library.arrangeTheBooks([5]), errMsg);
+            assert.throw(() => library.arrangeTheBooks(5.55), errMsg);
+            assert.throw(() => library.arrangeTheBooks(-5), errMsg);
+            assert.throw(() => library.arrangeTheBooks({}), errMsg);
+        });
+        it('should return a message if there is enough space and books are arranged', () => {
+            let returnMsg = `Great job, the books are arranged.`;
+            assert.equal(library.arrangeTheBooks(25), returnMsg);
+        });
+        it('should return a message if there is enough space and books are arranged', () => {
+            let returnMsg = `Great job, the books are arranged.`;
+            assert.equal(library.arrangeTheBooks(40), returnMsg);
+        });
+        it('should return a message if there is enough space and books are arranged', () => {
+            let returnMsg = `Insufficient space, more shelves need to be purchased.`;
+            assert.equal(library.arrangeTheBooks(41), returnMsg);
+            assert.equal(library.arrangeTheBooks(101), returnMsg);
+        });
+    });
 });
