e245d780728b93975a44f5a826b794a5785a26d7
MartinaPoriazova <102858702+MartinaPoriazova@users.noreply.github.com>
2022-10-18 21:49:13 +0300

    Updated one return message

--- Old_Exams/Ex_120821/02. Art Gallery/solve.js
+++ Old_Exams/Ex_120821/02. Art Gallery/solve.js
@@ -31,7 +31,7 @@ class ArtGallery {
         let presented = this.guests.find(guest => guest.guestName === guestName);
         if (presented) {
             throw new Error(`${guestName} has already been invited.`);
-        }
+        } 
 
         let points = 0;
 
@@ -60,6 +60,7 @@ class ArtGallery {
             if (exists.articleModel !== articleModel) {
                 throw new Error(`This article is not found.`);
             }
+
             if (exists.quantity === 0) {
                 return `The ${articleName} is not available.`;
             }
@@ -67,14 +68,14 @@ class ArtGallery {
 
         let presented = this.guests.find(guest => guest.guestName === guestName);
         if (!presented) {
-            throw new Error(`This guest is not invited.`);
+            return `This guest is not invited.`;
         } else {
             if (this.guests.points < this.possibleArticles[`${articleModel}`]) {
                 return `You need to more points to purchase the article.`;
             } else {
                 presented.points -= `${this.possibleArticles[`${articleModel}`]}`;
                 exists.quantity -= 1;
-                presented.purchaseArticle +=1;
+                presented.purchaseArticle += 1;
             }
         }
 
@@ -108,23 +109,23 @@ class ArtGallery {
 // console.log(artGallery.inviteGuest('Peter', 'Middle'));
 // console.log(artGallery.inviteGuest('John', 'Middle'));
 
-// const artGallery = new ArtGallery('Curtis Mayfield');
-// artGallery.addArticle('picture', 'Mona Liza', 3);
-// artGallery.addArticle('Item', 'Ancient vase', 2);
-// artGallery.addArticle('picture', 'Mona Liza', 1);
-// artGallery.inviteGuest('John', 'Vip');
-// artGallery.inviteGuest('Peter', 'Middle');
-// console.log(artGallery.buyArticle('picture', 'Mona Liza', 'John'));
-// console.log(artGallery.buyArticle('item', 'Ancient vase', 'Peter'));
-// console.log(artGallery.buyArticle('item', 'Mona Liza', 'John'));
-
-const artGallery = new ArtGallery('Curtis Mayfield'); 
+const artGallery = new ArtGallery('Curtis Mayfield');
 artGallery.addArticle('picture', 'Mona Liza', 3);
 artGallery.addArticle('Item', 'Ancient vase', 2);
 artGallery.addArticle('picture', 'Mona Liza', 1);
 artGallery.inviteGuest('John', 'Vip');
 artGallery.inviteGuest('Peter', 'Middle');
-artGallery.buyArticle('picture', 'Mona Liza', 'John');
-artGallery.buyArticle('item', 'Ancient vase', 'Peter');
-console.log(artGallery.showGalleryInfo('article'));
-console.log(artGallery.showGalleryInfo('guest'));
+console.log(artGallery.buyArticle('picture', 'Mona Liza', 'John'));
+console.log(artGallery.buyArticle('item', 'Ancient vase', 'Peter'));
+console.log(artGallery.buyArticle('item', 'Mona Liza', 'John'));
+
+// const artGallery = new ArtGallery('Curtis Mayfield'); 
+// artGallery.addArticle('picture', 'Mona Liza', 3);
+// artGallery.addArticle('Item', 'Ancient vase', 2);
+// artGallery.addArticle('picture', 'Mona Liza', 1);
+// artGallery.inviteGuest('John', 'Vip');
+// artGallery.inviteGuest('Peter', 'Middle');
+// artGallery.buyArticle('picture', 'Mona Liza', 'John');
+// artGallery.buyArticle('item', 'Ancient vase', 'Peter');
+// console.log(artGallery.showGalleryInfo('article'));
+// console.log(artGallery.showGalleryInfo('guest'));
